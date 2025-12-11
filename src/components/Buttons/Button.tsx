252dcddd147fce43a7b2aab877eb604aa6127e2d
Gallucky <galbenabu1111@email.com>
2025-12-12 00:37:04 +0200

    fix(deps): Update and regenerate package-lock.json
    Regenerate the lockfile following major dependency updates and potential changes in the development environment.

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,5 +1,16 @@
-const Button = () => {
-    return <></>;
+type ButtonProps = {
+    children?: React.ReactNode;
+    className?: string;
+};
+
+const Button = (props: ButtonProps) => {
+    const { children, className = "" } = props;
+
+    return (
+        <>
+            <button className={`rounded-2xl bg-white ${className}`}>{children}</button>
+        </>
+    );
 };
 
 export default Button;
